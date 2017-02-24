149280e48fd493d237b76a631571b71c639130b7
zhangjw@mmwipo.com <zhangjw@mmwipo.com>
2017-02-24 09:37:19 +0800

    Initial Commit

---
+++ TRZXPersonalCustomerCenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TRZXPersonalCustomerCenter
+//
+//  Created by 张江威 on 2017/2/24.
+//  Copyright © 2017年 张江威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
